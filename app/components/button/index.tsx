8e69d351c03526dda1c5de018607537fabe84feb
Ivan Yeromenko <johnyeremenko91@gmail.com>
2024-02-07 12:27:59 +0100

    Add vanilla-extract 🍦

---
+++ app/components/button/index.tsx
@@ -0,0 +1,7 @@
+import * as styles from "./styles.css";
+
+const Button = () => {
+  return <button className={styles.root}>Button</button>;
+};
+
+export default Button;
---
+++ app/components/button/styles.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+
+export const root = style({
+  border: "solid 1px red",
+  background: "white",
+  color: "#454545",
+});
